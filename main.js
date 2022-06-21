4495cfbf7ba74a484bf9aeafca93b46522219df7
MaheshRath08 <maheshrath.2002@gmail.com>
2022-06-21 20:59:32 +0530

    added some comments

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ let t = document.getElementById("t")
 let btnEl = document.getElementById("btn")
 
 btnEl.addEventListener("click", function(){
-        let SI = ((p.value)*(r.value)*(t.value))/100
-        if (isNaN(SI)){
-            mainEl.innerHTML = `<h1>Type in Numerical Value</h1> 
+        let SI = ((p.value)*(r.value)*(t.value))/100                           //SI = prt/100
+        if (isNaN(SI)){                                                     //checking if SI is a number or not
+            mainEl.innerHTML = `<h1>Type in Numerical Value</h1>            
                             <button onclick="location.reload()">Back</button>`
-        }else{
+        }else{                                                              //to ensure all the input values are numericals
             mainEl.innerHTML = `<h1>Simple Interest is ${SI}</h1> 
                                 <button onclick="location.reload()">Back</button>`
         }                        
